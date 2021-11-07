fe9b50d303833c88e5e7d0a32a6ad907f3ba7f51
Tehila Rozin <tehila.rozin@genesys.com>
2021-11-07 20:25:16 +0200

    updating app with renamed module (genesys-mobiledx-chat-rn-module) artifact

--- ChatButton.js
+++ ChatButton.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { NativeModules, Button } from 'react-native';
 
-import DXChatModule from 'react-native-android-module-gndx';
+import GenDXChatModule from 'genesys-mobiledx-chat-rn-module';
 
 
 const ChatButton = () => {
   const onPress = () => {
 
-    DXChatModule.startChatComponent("f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f", "inindca.com",
+    GenDXChatModule.startChatComponent("f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f", "inindca.com",
     "com.genesys.messenger.poc", true, "tehila.rozin@genesys.com", "666-666-66", "Fame", "RO");
   }
 
